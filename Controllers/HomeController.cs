5a435e5288e552b12629d9b95aa9923796ba5baf
shunjid1862 <shunjid1862@gmail.com>
2019-05-10 16:07:30 +0600

    added user-tracker for Home/Index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 using routine_explorer.Data;
 using routine_explorer.Models;
 
@@ -20,6 +22,18 @@ namespace routine_explorer.Controllers
         }
         public async Task<IActionResult> Index()
         {
+            HttpClient http = new HttpClient();
+            var data = http.GetAsync("https://geoip-db.com/json/").Result.Content.ReadAsStringAsync().Result;
+            JObject objectData = JObject.Parse(data);
+            Audit audit = new Audit();
+            audit.UserIP = (string)objectData["IPv4"];
+            audit.UserLocation = (string)objectData["latitude"] + "," + (string)objectData["longitude"] + "," + (string)objectData["city"];
+            audit.AreaAccessed = "/Home/Index";
+            audit.ActionDateTime = DateTime.Now;
+            _context.Add(audit);
+            await _context.SaveChangesAsync();
+            _context.Remove(audit);
+
             var lastAddedId = _context.RoutineFileUploaderStatus.OrderByDescending(p => p.TimeOfUpload).FirstOrDefault();
             var courses = await _context.Routine
             .OrderBy(n => n.CourseCode)
